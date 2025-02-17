a4bb68bc5997c83c3c8e610ad0f56ecb63544712
Shikhar0895 <shikhar0895@gmail.com>
2025-02-17 22:31:40 +0530

    changed app name

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Navbar />
-      <h1 className="font-bold text-center">Welcome to Brainstorm</h1>
+      <h1 className="font-bold text-center">Welcome to Articulate</h1>
       <p className="text-center text-foreground/50">
         Your very own whiteboard solution with 100x efficiency collaboration.
       </p>
